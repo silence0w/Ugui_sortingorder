01744d2332cc5c9f08f33f6a8deb31f9e97464e4
silence0w <2744004266@qq.com>
2018-06-12 20:54:41 +0800

    ugui_sortingorder version_01

---
+++ Assets/Uidepth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// UI的特效叠层控制
+/// </summary>
+public class Uidepth : MonoBehaviour {
+
+    public int depthorder;//设置绘制深度
+    public bool isUI=true;//判断是否为ui组件或者是粒子特效
+                         
+    void Start () {
+
+        if (isUI)
+        {//是ui
+            Canvas canvas=GetComponent<Canvas>();
+            if (canvas==null)
+            {//Canvas不存在时
+                canvas= this.gameObject.AddComponent<Canvas>();//绑定canvas
+            }
+            canvas.overrideSorting = true;//允许绘制排序  忽略ugui的默认父子级渲染次序
+            canvas.sortingOrder = depthorder;//设置绘制深度
+
+
+        }
+        else
+        {//是粒子特效
+            Renderer[] render = this.GetComponentsInChildren<Renderer>();//获取所有的Renderer组件
+            foreach (Renderer item in render)
+            {
+                item.sortingOrder = depthorder;//设置绘制深度
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
